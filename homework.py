2d13435a7ba0d6111dcce2036bac5b013d6dd717
sovushka <sovushka.sever@gmail.com>
2020-05-01 20:43:32 +0300

    fixed another review remarks

--- homework.py
+++ homework.py
@@ -32,8 +32,9 @@ class Record:
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         total_amount = self.get_today_stats()
+        balance = self.limit - total_amount
         if total_amount < self.limit:
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {(self.limit - total_amount):.0f} кКал'
+            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance:.0f} кКал'
         elif total_amount >= self.limit:
             return 'Хватит есть!'
 
@@ -44,16 +45,17 @@ class CashCalculator(Calculator):
     EURO_RATE = 88.75
 
     def get_today_cash_remained(self, currency):
-        currency_name = { "rub": ["руб", 1],
-                          "usd": ["USD", self.USD_RATE],
-                          "eur": ["Euro", self.EURO_RATE] }
+        currency_name = { "rub": ("руб", 1),
+                          "usd": ("USD", self.USD_RATE),
+                          "eur": ("Euro", self.EURO_RATE) }
+        
         total_amount = self.get_today_stats()
         if currency in currency_name:
+            currensy_presentation, exchange_rate = currency_name[currency]
+            balance = abs((self.limit - total_amount)/exchange_rate)
             if total_amount < self.limit:
-                return f'На сегодня осталось {(self.limit - total_amount)/currency_name[currency][1]:.2f} {currency_name[currency][0]}'            
+                return f'На сегодня осталось {balance:.2f} {currensy_presentation}'
             elif total_amount == self.limit:    
                 return 'Денег нет, держись'
             else:      
-                return f'Денег нет, держись: твой долг - {(total_amount - self.limit)/currency_name[currency][1]:.2f} {currency_name[currency][0]}'
-
-
+                return f'Денег нет, держись: твой долг - {balance:.2f} {currensy_presentation}'
