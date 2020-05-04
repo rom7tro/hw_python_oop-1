766e341fc53dda8997b5d63392373ef056c608b9
sovushka <sovushka.sever@gmail.com>
2020-05-04 14:48:46 +0300

    Refactor due to PEP8 errors, rename variables.

--- homework.py
+++ homework.py
@@ -9,7 +9,7 @@ class Calculator:
     def get_week_stats(self):
         now = dt.datetime.now().date()
         week_ago = now - dt.timedelta(days=7)
-        amount = [record.amount for record in self.records if week_ago <= record.date ]
+        amount = [record.amount for record in self.records if week_ago <= record.date]
         return sum(amount)
 
     def add_record(self, record):
@@ -17,9 +17,10 @@ class Calculator:
 
     def get_today_stats(self):
         now = dt.datetime.now().date()
-        amount = [record.amount for record in self.records if now == record.date ]
+        amount = [record.amount for record in self.records if now == record.date]
         return sum(amount)
     
+
 class Record:
     def __init__(self, amount, comment, date=dt.datetime.now().date()):
         self.amount = amount
@@ -28,6 +29,7 @@ class Record:
         if isinstance(date, str):
             date = dt.datetime.strptime(date, date_format).date()
         self.date = date
+
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
@@ -45,17 +47,17 @@ class CashCalculator(Calculator):
     EURO_RATE = 88.75
 
     def get_today_cash_remained(self, currency):
-        currency_name = { "rub": ("руб", 1),
-                          "usd": ("USD", self.USD_RATE),
-                          "eur": ("Euro", self.EURO_RATE) }
-        
+        currency_name = {"rub": ("руб", 1),
+                         "usd": ("USD", self.USD_RATE),
+                         "eur": ("Euro", self.EURO_RATE)}
+
         total_amount = self.get_today_stats()
         if currency in currency_name:
-            currensy_presentation, exchange_rate = currency_name[currency]
-            balance = abs ((self.limit - total_amount) / exchange_rate)
+            currency_presentation, exchange_rate = currency_name[currency]
+            balance = abs((self.limit - total_amount) / exchange_rate)
             if total_amount < self.limit:
-                return f'На сегодня осталось {balance:.2f} {currensy_presentation}'
+                return f'На сегодня осталось {balance:.2f} {currency_presentation}'
             elif total_amount == self.limit:    
                 return 'Денег нет, держись'
             else:      
-                return f'Денег нет, держись: твой долг - {balance:.2f} {currensy_presentation}'
+                return f'Денег нет, держись: твой долг - {balance:.2f} {currency_presentation}'
